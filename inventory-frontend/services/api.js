3dfb1b7630734f94090e0ab99a8f5aeee1068a9b
Venkateswarlu Vennampalli <venkateswarlu4466@gmail.com>
2026-06-10 20:13:49 +0530

    Fix API URL

--- inventory-frontend/services/api.js
+++ inventory-frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ims-final-project.onrender.com/api/",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://ims-final-project.onrender.com/api/",
 });
 
 export default API;
